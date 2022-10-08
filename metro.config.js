1915f4dd021df36392327b6569eac0db9201d474
Yanggz <350926623@qq.com>
2022-10-08 11:23:21 +0800

    stop zzz...

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.transformer.minifierPath = 'metro-minify-terser';
-config.transformer.minifierConfig = {
-  // Terser options...
-};
-
-module.exports = config
+// // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
+//
+// const config = getDefaultConfig(__dirname);
+//
+// config.transformer.minifierPath = 'metro-minify-terser';
+// config.transformer.minifierConfig = {
+//   // Terser options...
+// };
+//
+// module.exports = config
